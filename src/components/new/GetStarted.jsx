315d529f8de92b32776491eb9b419cac4e531df2
YasserDalali <yasser.dalali.personal@gmail.com>
2024-10-26 16:24:37 +0100

    changed color pallette

--- src/components/new/GetStarted.jsx
+++ src/components/new/GetStarted.jsx
@@ -2,11 +2,22 @@ import React from 'react'
 import styles from '../../style';
 import { arrowUp } from '../../assets';
 
+/**
+ * Returns a "Get Started" button component
+ * 
+ * This component is a rounded rectangle with a blue gradient background and a white 
+ * foreground. It has two lines of text that are centered horizontally and vertically 
+ * inside the rectangle. The first line of text is "Get" in a blue gradient color, and the 
+ * second line of text is "Started" in the same color. The component also has an upward 
+ * pointing arrow icon to the right of the text.
+ * 
+ * @return {ReactElement} The "Get Started" button component
+ */
 function GetStarted() {
   return (
-    <div className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-blue-gradient cursor-pointer`}>
+    <div className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-blue-gradient cursor-pointer hover:scale-110 hover:shadow-lg hover:shadow-secondary transition-all`}>
 
-      <div className={`${styles.flexCenter} flex-col bg-primary w-[95%] h-[95%] rounded-full`}>
+      <div className={`${styles.flexCenter} flex-col h bg-primary w-[95%] h-[95%] rounded-full`}>
         <div className={`${styles.flexStart} flex-row`}>
 
           <p className='font-poppins font-medium text-[18px] leading-[23.4px]'>
--- src/pages/SecondLanding.jsx
+++ src/pages/SecondLanding.jsx
@@ -3,45 +3,56 @@ import styles from "../style";
 import {NavBar, Hero, Billing, CardDeal, Business, Clients, CTA, Footer, Stats, Testimonials} from "../components/new/index";
 
 
+/**
+ * Returns a Landing page component
+ * 
+ * This component is the second landing page of the website, and it contains the following sub-components:
+ * - NavBar
+ * - Hero
+ * - Stats
+ * - Business
+ * - Billing
+ * - CardDeal
+ * - Testimonials
+ * - Clients
+ * - CTA
+ * - Footer
+ * 
+ * The component also has a background color of #1A202C and a full width of the screen.
+ * The component is also overflow-hidden, which means that any content that is larger than the component
+ * will be cut off at the edges of the component.
+ * 
+ * @return {ReactElement} The Landing page component
+ */
 function SecondLanding() {
   return (
-    <div className="bg-primary w-full overflow-hidden text-white">
-      
-            
-      <nav className={`${styles.paddingX} ${styles.flexCenter}`}>
-        <div className={`${styles.boxWidth}`}>
-            <NavBar />
-        </div>
-      </nav>
-
-
-
-      <main className={`bg-primary ${styles.flexStart}`}>
-        <article className={`${styles.boxWidth}`}>
-            <Hero />
-            <Stats />
-            <Billing /> 
-            <CardDeal />
-            <Testimonials />
-            <CTA />
-            <Footer />
-        </article>
-
-
-        
-        <article className={`${styles.paddingX} ${styles.flexStart} bg-primary`}>
-          <div className={`${styles.boxWidth}`}>
-
-            
-            
-
-          </div>
-        </article>
-
-
-      </main>
+    <div className="bg-primary w-full overflow-hidden">
+    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth} text-white`}>
+        <NavBar />
+      </div>
     </div>
-  );
+
+    <div className={`bg-primary ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Hero />
+      </div>
+    </div>
+    
+    <div className={`bg-primary ${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Stats />
+        <Business />
+        <Billing />
+        <CardDeal />
+        <Testimonials />
+        <Clients />
+        <CTA />
+        <Footer />
+      </div>
+    </div>
+  </div>
+);
 }
 
 export default SecondLanding;
